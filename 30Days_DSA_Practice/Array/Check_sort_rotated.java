b705f9c74f65e079ddf00a392557b5d94beb74a3
codermeghagithub <meghade2003@gmail.com>
2025-08-24 17:48:17 +0530

    Day3 of dsa leetcode question

---
+++ 30Days_DSA_Practice/Array/Check_sort_rotated.java
@@ -0,0 +1,48 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class Check_sort_rotated {
+    public static boolean issort_rotated(int[] nums) {
+        int count = 0;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            // If order breaks more than once, array is not sorted/rotated.
+            if (nums[i] > nums[(i + 1) % len]) {
+                count++;
+            }
+        }
+        return count <= 1;
+
+    }
+
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enetr the size of arr:");
+        n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter ele in arr:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        if (issort_rotated(nums)) {
+            System.out.println("array is  sorted ");
+        } else {
+            System.out.println("Array is not sorted");
+        }
+
+        if (issort_rotated(nums)) {
+            System.out.println("Array is already sorted/rotated.");
+        } else {
+            System.out.println("Array is not sorted, sorting now...");
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length; i++) {
+                System.out.println(nums[i]);
+            }
+            System.out.println("Sorted Array: " + Arrays.toString(nums));
+        }
+
+        // Step 2: Sort the array
+    }
+
+}
--- Data Stucture/Sort-sarch/Bubble_sort.java
+++ Data Stucture/Sort-sarch/Bubble_sort.java
@@ -1,48 +1,55 @@
+
 import java.util.Scanner;
+
 public class Bubble_sort {
 
-  public static boolean isSorted(int[] arr){
-    for(int i=0;i<arr.length-1;i++){
-      if(arr[i]>arr[i+1]){
-        return false;
-      }
+    public static boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
-      return true;
-  }
-  public static void main(String[] args){
-    int n; 
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enetr the size of arr:");
-    n=sc.nextInt();
-    int[] arr=new int[n];
-    System.out.println("Enter ele in arr:");
-    for(int i=0;i<n;i++){
-    arr[i]=sc.nextInt();
-  }
-System.out.println("Length: "+arr.length);
-  // ** Check before sorting
-  if(isSorted(arr)){
-  System.out.println("Array is already sorted.");
-  }
-else{
-  System.out.println("Array is not sorted.");
-}
-  for (int i=0;i<n-1;i++){
-    boolean swapped=false;
-    for(int j=0;j<n-1-i;j++){
-      if(arr[j]>arr[j+1]){
-      int temp=arr[j];
-      arr[j]=arr[j+1];
-      arr[j+1]=temp;
-      swapped=true;
-    } }
-            if(!swapped)
-              break;   //**stop is alread sorted
-  }
-  // **Print sorted array
+
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enetr the size of arr:");
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter ele in arr:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        System.out.println("Length: " + arr.length);
+        // ** Check before sorting
+        if (isSorted(arr)) {
+            System.out.println("Array is already sorted.");
+        } else {
+            System.out.println("Array is not sorted.");
+        }
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {  // **check inside bubble sort to stop unnecessary extra passes.  if we not write it's fine but this line make this code optimize 
+                break;   //**stop is alread sorted
+
+                    }}
+        // **Print sorted array
         System.out.println("Sorted array:");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
-  
-} }
+
+    }
+}
