9cb7f4c364a7d4d16a3bba893e6352a3c37eca4d
codermeghagithub <meghade2003@gmail.com>
2025-09-22 23:57:42 +0530

    add new file

--- 30Days_DSA_Practice/Array/Duplicate_ele.java
+++ 30Days_DSA_Practice/Array/Duplicate_ele.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Duplicate_ele {
-  
+  public static void main(String[] args) {
+      int n;
+System.out.println("Enter size of array:");
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int[] nums=new int [n];
+System.out.println("enter ele:");
+for(int  i=0;i<n;i++)
+{
+  nums[i]=sc.nextInt();
+}
+
+  }
 }
