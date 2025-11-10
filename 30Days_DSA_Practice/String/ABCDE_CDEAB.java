4479703a6217945a35a72ec8dc57edf79f82bd83
codermeghagithub <meghade2003@gmail.com>
2025-11-10 21:28:18 +0530

    also add leetcode solution

--- 30Days_DSA_Practice/String/ABCDE_CDEAB.java
+++ 30Days_DSA_Practice/String/ABCDE_CDEAB.java
@@ -1,5 +1,6 @@
 
-
+// **Input: s = "abcde", goal = "cdeab"
+// Output: true
 import java.util.Scanner;
 
 public class ABCDE_CDEAB {
@@ -37,9 +38,38 @@ char[] ch1 = s.toCharArray();
 }
 
 
-// **
+// ** leetcode 
 
+// class Solution{
+//     private void ReverseStr(char[] ch1,int l,int r){
+//         while(l<r){
+//             char temp=ch1[l];
+//             ch1[l]=ch1[r];
+//             ch1[r]=temp;
+//             l++;
+//             r--;
+//         }
 
+//     }
+//     public boolean rotateString(String s, String goal) {
+//         int n=s.length();
+//         if(n!=goal.length()){
+//             return false;
+//         }
+//         for(int k=0;k<=n-1;k++){
+//             char[] ch1=s.toCharArray();
+//             ReverseStr(ch1,0,k-1);
+//             ReverseStr(ch1,k,n-1);
+//             ReverseStr(ch1,0,n-1);
+//             String res=new String(ch1);
+        
+//         if(res.equals(goal)){
+//             return true;
+//         }}
+//         return false;
+
+//     }
+// }
 
 // **
 
