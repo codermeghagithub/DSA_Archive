27ec0f366515e33ca6e0db32412f1f083659a405
codermeghagithub <meghade2003@gmail.com>
2025-11-10 20:18:14 +0530

    Rotate string leetcode-796 number question is solved

--- 30Days_DSA_Practice/String/ABCDE_CDEAB.java
+++ 30Days_DSA_Practice/String/ABCDE_CDEAB.java
@@ -1,3 +1,92 @@
+
+
+import java.util.Scanner;
+
 public class ABCDE_CDEAB {
-  
+
+    static void Reverse(char[] ch1, int l, int r) {
+        while (l < r) {
+            char temp = ch1[l];
+            ch1[l] = ch1[r];
+            ch1[r] = temp;
+            l++;
+            r--;
+        }
+
+    }
+
+    static void Rotate(char[] ch1, int k) {
+        int n = ch1.length;
+        k = k % n;
+        Reverse(ch1, 0, k -1);
+        Reverse(ch1, k, n - 1);
+        Reverse(ch1, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter ant string");
+        String s = sc.nextLine();
+        
+        System.out.println("Enter K value:");
+        int k = sc.nextInt();
+char[] ch1 = s.toCharArray();
+        Rotate(ch1, k);
+ String res = new String(ch1);
+        System.out.println("After rotation the string :" + res);    }
 }
+
+
+// **
+
+
+
+// **
+
+// ** how to roteated or reverse a string using 2 pointer 
+// import java.util.Scanner;
+// public class Rotate_str {
+//   public static void main(String[] args){
+// Scanner sc=new Scanner(System.in);
+// System.out.println("Enter ant string");
+// String s=sc.nextLine();
+// char[] ch1=s.toCharArray();
+// int left=0;
+// int right=ch1.length-1;
+// while(left<right){
+//     char temp=ch1[left];
+//     ch1[left]=ch1[right];
+//     ch1[right]=temp;
+//     left++;
+//     right--;
+// }
+// String reverse=new String(ch1);
+// System.out.println("After rotation the string   :"+reverse);
+//   }
+// }
+// import java.util.Scanner;
+// public class Main {
+//     static boolean isrotatestr(String s) {
+//         char[] ch1=s.toCharArray();
+//         int left=0;
+//         int right =ch1.length-1;
+//         while(left<right){
+//     char temp=ch1[left];
+//     ch1[left]=ch1[right];
+//     ch1[right]=temp;
+//     left++;
+//     right--;
+//       }
+//         String revered=new String(ch1);
+//         System.out.println("After rotation the string   :"+revered);
+//           // Check if reversed == original
+//         return s.equals(revered);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter any string:");
+//         String s = sc.nextLine();
+//         boolean result=isrotatestr(s);
+//         System.out.println("After rotation we have to check pallindrom nither i can't retun boolen value  :"+result); // **After rotation we have to check pallindrom nither i can't retun boolen value  :true
+//     }
+// }
