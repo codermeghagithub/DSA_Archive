dcfe346d9987e5317de79f64eb418e62dabdba9d
codermeghagithub <meghade2003@gmail.com>
2025-11-21 23:32:28 +0530

    working on Back space compare

---
+++ 30Days_DSA_Practice/String/Back_Space.java
@@ -0,0 +1,28 @@
+// **Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+// Note that after backspacing an empty text, the text will continue empty.
+import java.util.Scanner;
+
+public class Back_Space {
+
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder();
+
+        System.out.print("Enter first word: ");
+        String s1 = sc.nextLine();
+
+        System.out.print("Enter second word: ");
+        String s2 = sc.nextLine();
+
+        // for(int i=0;i<sb.length();i++){
+        // if(sb.charAt(i)=='#'){
+        int l = 0;
+        int r = sb.length();
+        while (l < r) {
+
+        }
+    }
+
+}
