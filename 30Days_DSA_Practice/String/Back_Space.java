865fc656e91ca1d34048e4914061074c7ace22a5
codermeghagithub <meghade2003@gmail.com>
2025-11-22 10:23:53 +0530

    solved 844number  leet code question

--- 30Days_DSA_Practice/String/Back_Space.java
+++ 30Days_DSA_Practice/String/Back_Space.java
@@ -1,9 +1,63 @@
 // **Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
 
 // Note that after backspacing an empty text, the text will continue empty.
-import java.util.Scanner;
+// import java.util.Scanner;
+// public class Back_Space{
 
+//     public static void main(String[] args) {
+//           Scanner sc = new Scanner(System.in);
+
+//         StringBuilder sb = new StringBuilder();
+
+//         System.out.print("Enter first word: ");
+//         String s1 = sc.nextLine();
+//         int hash_count=0;
+//         StringBuilder actual_str=new StringBuilder();
+//         for(int i=s1.length()-1;i>=0;i--){
+//             if(s1.charAt(i)=='#'){
+//                 hash_count++;
+//                 // continue; // **No continue is needed because after finishing the if (hash_count > 0) block,
+// //** the loop automatically moves to the next iteration(end of the loop) [i--]. 
+//             }
+//             if(hash_count>0){ // hash_count>0
+//              hash_count--;
+//              continue;
+//             }
+//             else{
+//                 actual_str.insert(0,s1.charAt(i));
+//             }
+//             }
+//             System.out.println(actual_str);
+//         }
+//     }
+
+
+
+// **
+
+import java.util.Scanner;
 public class Back_Space {
+    
+    
+    public static String convert_act_str(String str){
+        
+        int hash_count=0;
+        StringBuilder actual_str=new StringBuilder();
+        for(int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i)=='#'){
+                hash_count++;
+                continue;
+            }
+            if(hash_count>0){
+                hash_count--;
+            }
+            else{
+                actual_str.insert(0,str.charAt(i));
+            }
+           
+        }
+        return actual_str.toString();
+    }
 
     public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
@@ -12,17 +66,14 @@ public class Back_Space {
 
         System.out.print("Enter first word: ");
         String s1 = sc.nextLine();
-
-        System.out.print("Enter second word: ");
+           System.out.print("Enter second word: ");
         String s2 = sc.nextLine();
-
-        // for(int i=0;i<sb.length();i++){
-        // if(sb.charAt(i)=='#'){
-        int l = 0;
-        int r = sb.length();
-        while (l < r) {
-
-        }
+        
+        String s3=convert_act_str(s1);
+        String s4=convert_act_str(s2);
+        System.out.print(s3.equals(s4));
+        
     }
-
 }
+        
+        
--- 30Days_DSA_Practice/String/Sum_substr.java
+++ 30Days_DSA_Practice/String/Sum_substr.java
@@ -2,44 +2,44 @@
 
 // **q3 . sum of all substring of a number  6759 =8421
 
-import java.util.Scanner;
-
-public class Sum_substr {
-  public static void main(String[] args){
-    int sum=0;
-Scanner sc = new Scanner(System.in);
-  System.out.print("Enter any number: ");
-   String num=sc.nextLine();
-   for(int i=0;i<num.length();i++){
-    for(int j=i+1;j<=num.length();j++){
-      String sub=num.substring(i,j);
-      int str_num=Integer.parseInt(sub);
-         sum=sum+str_num;
-    }
-   }
-   System.out.println("sum of all subString number is :"+sum);  // * O(n)2
-     }}
-
-
-// **OPTIMISE
-
 // import java.util.Scanner;
 
 // public class Sum_substr {
 //   public static void main(String[] args){
-
-// long total=0;
-// long prev=0;
-//     Scanner sc = new Scanner(System.in);
+//     int sum=0;
+// Scanner sc = new Scanner(System.in);
 //   System.out.print("Enter any number: ");
-//   String num=sc.nextLine();
-//   for(int i=0;i<num.length();i++){
-//   int num2=num.charAt(i)-'0';
-//   long current=prev*10+(long)(i+1)*num2;
-//    total=total+current;
-// prev=current;
-//   //**  System.out.println(num2);
-    
+//    String num=sc.nextLine();
+//    for(int i=0;i<num.length();i++){
+//     for(int j=i+1;j<=num.length();j++){
+//       String sub=num.substring(i,j);
+//       int str_num=Integer.parseInt(sub);
+//          sum=sum+str_num;
+//     }
 //    }
-//    System.out.println("sum of all subString number is :"+total);  // * O(n)
+//    System.out.println("sum of all subString number is :"+sum);  // * O(n)2
 //      }}
+
+
+// **OPTIMISE
+
+import java.util.Scanner;
+
+public class Sum_substr {
+  public static void main(String[] args){
+
+long total=0;
+long prev=0;
+    Scanner sc = new Scanner(System.in);
+  System.out.print("Enter any number: ");
+  String num=sc.nextLine();
+  for(int i=0;i<num.length();i++){
+  int num2=num.charAt(i)-'0';
+  long current=prev*10+(long)(i+1)*num2;
+   total=total+current;
+prev=current;
+  //**  System.out.println(num2);
+    
+   }
+   System.out.println("sum of all subString number is :"+total);  // * O(n)
+     }}
