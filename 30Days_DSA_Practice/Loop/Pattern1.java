eeec6b70fe632dca234f4e2d307d8028311e8e52
codermeghagithub <meghade2003@gmail.com>
2025-12-21 17:21:52 +0530

    add 3 basic patterns code

--- 30Days_DSA_Practice/Loop/Pattern1.java
+++ 30Days_DSA_Practice/Loop/Pattern1.java
@@ -1,7 +1,71 @@
+//  **HOLLOW RECTANGLE
+import java.util.Scanner;
+public class Pattern1
+{
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the number of rows and column:");
+	int  r=sc.nextInt();
+	int c=sc.nextInt();
+	for(int i=0;i<r;i++){
+	    for(int j=0;j<c;j++){
+	        if(i==j||i==(r-1)||(j==0)||(j==c-1)){
+	        System.out.print("*");
+	        }else{
+	             System.out.print(" ");
+	        }
+	    }
+	    System.out.println("");
+	}
 
-public class Pattern1 {
-  public static void main(String[] args) {
-      
-      
-  }
 }
+}
+// *
+// **
+// ***
+// ****
+// import java.util.Scanner;
+// import java.lang.Math; 
+
+// public class Pattern1
+// {
+// public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+// 	System.out.println("Enter the number of rows and column:");
+// 	int  r=sc.nextInt();
+// 	int c=sc.nextInt();
+// 	for(int i=0;i<r;i++){
+// 	    for(int j=0;j<i;j++){
+	        
+// 	    System.out.print("*");
+// 	}
+// 	   System.out.println("");  
+// 	}
+
+// }
+// }
+
+
+// import java.util.Scanner;
+
+// public class Main
+// {
+// public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+// 	System.out.println("Enter the number of rows and column:");
+// 	int  r=sc.nextInt();
+// 	int c=sc.nextInt();
+// // 	****
+// // 	 ***
+// // 	  **
+// // 	   *
+// 	for(int i=r;i>0;i--){
+// 	    for(int j=0;j<i;j++){  // *i=4,j=4 4>=4 3>=4 i=3 
+	        
+// 	    System.out.print("*");
+// 	}
+// 	   System.out.println("");  
+// 	}
+
+// }
+// }
