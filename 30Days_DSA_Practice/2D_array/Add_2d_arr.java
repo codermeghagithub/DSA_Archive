dc4571b57458ce1fade5b3d2735253b41a8bc681
codermeghagithub <meghade2003@gmail.com>
2025-12-21 05:31:19 +0530

    addition of 2 matrices

---
+++ 30Days_DSA_Practice/2D_array/Add_2d_arr.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+public class Add_2d_arr
+{
+    static void Display_Matrix(int[][] sum){
+        System.out.print("2Dd arr print:");
+
+
+        // **🧠 Why Java does NOT allow this
+// int r1;   // declared
+
+
+// But you never did:
+
+// r1 = 3;   // initialized
+         for(int i=0;i<sum.length;i++){
+            for(int j=0;j<sum[i].length;j++){
+                
+                System.out.print(sum[i][j]+" ");
+        
+    }
+         System.out.println("") ;   
+         }}
+    
+    static void Addition(int[][]arr,int r1,int c1,int[][]arr2,int r2,int c2){
+    if(r1!=r2 || c1!=c2){
+        System.out.println("Addition not possible😔");
+        return;
+    }
+    int s1,s2;
+    int[][] sum=new int[r1][c1];
+    for(int i=0;i<r1;i++){ // ROW
+        for(int j=0;j<c1;j++){ //COLOUMN
+        sum[i][j]=arr[i][j]+arr2[i][j];
+        }
+    }
+    Display_Matrix(sum);
+  
+    }
+    
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+      
+        // Get dimensions from user 2st ARRAY
+       System.out.print("Enter number of rows &  columns: ");
+        int r1 = sc.nextInt();
+        int c1 = sc.nextInt();
+        
+          int[][] arr=new int[r1][c1];
+       System.out.print("Enter the ele of arr:") ;  
+        for(int i=0;i<r1;i++){
+            for(int j=0;j<c1;j++){
+                arr[i][j]=sc.nextInt();
+                         }}
+                         
+// 2nd ARRAY  
+                         
+    System.out.print("Enter number of rows &  columns: ");
+        int r2 = sc.nextInt();
+        int c2 = sc.nextInt();
+        
+          int[][] arr2=new int[r2][c2];
+       System.out.print("Enter the ele of arr:") ;  
+        for(int i=0;i<r2;i++){
+            for(int j=0;j<c2;j++){
+                arr2[i][j]=sc.nextInt();
+       
+                         }}
+     System.out.println("Matrix1 :");
+    Display_Matrix(arr);
+    System.out.println("Matrix1 :");
+    Display_Matrix(arr2);
+Addition(arr,r1,c1,arr2,r2,c2);
+
+ 
+	}
+}
--- 30Days_DSA_Practice/Hashmap/Anagram.java
+++ 30Days_DSA_Practice/Hashmap/Anagram.java
@@ -1,6 +1,54 @@
+
+// *LENGTH HAS TO EQUAL
 //  *The frequency of each character must be the same.  here frequency means : number of times a character occurs in a string
+//  *NO CHARECTER SHOULD BE DIFFERENT OR EXTRA 
+// ✅ Is
+// ip = "felt"
+// op = "telf"
+// an anagram?
+// ✔ YES, it is an anagram.
+// *❌ No, an anagram does NOT always give a meaningful word.
 
 
-public class Anagram {
-  
-}
+// import java.util.HashMap;
+// import java.util.Scanner;
+
+// public class Anagram {
+
+//    public static void Is_anagram(String s,String t){
+// if(s.length()!=t.length()){
+//    System.out.println("Not anagram");
+//     return;
+// }
+// HashMap<Character,Integer>map=new HashMap<>();
+// for(char ch:s.toCharArray()){
+//    map.put(ch,map.getOrDefault(ch,0)+1);
+
+// }
+// for (char ch : s2.toCharArray()) {
+//             if (!map.containsKey(ch)) {
+//                 System.out.println("Not an Anagram");
+//                 return;
+//             }
+//             map.put(ch, map.get(ch) - 1);
+
+//             if (map.get(ch) == 0) {
+//                 map.remove(ch);
+//             }
+//         }
+
+//         System.out.println(map.isEmpty() ? "Valid Anagram" : "Not an Anagram");
+//    }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter first string: ");
+//         String s = sc.nextLine();
+
+//         System.out.print("Enter second string: ");
+//         String t = sc.nextLine();
+// s=s.replace("\\s", "").toLowerCase();
+// t=t.replace("\\s","").toLowerCase();
+
+
+// }
+// }
