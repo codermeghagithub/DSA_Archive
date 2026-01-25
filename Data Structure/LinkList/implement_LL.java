eb35d8e22c40335ad9c1968398461f13aa005791
codermeghagithub <meghade2003@gmail.com>
2026-01-25 19:30:37 +0530

    add Node class implementation to linked list

--- Data Structure/LinkList/implement_LL.java
+++ Data Structure/LinkList/implement_LL.java
@@ -1,6 +1,15 @@
 public class implement_LL {
+  public static class Node{
+    int data;
+    Node next;
+    Node(int data){
+      this.data=data;
+    }
+  }
   
   public  static void main(String[] args){
+   Node n1=new Node(1); 
+   System.out.println(n1.next); // o/p=null
 
   }
 }
