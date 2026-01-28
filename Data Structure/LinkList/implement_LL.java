9391d81a121bf66a2ce08c586adae23132e96f05
codermeghagithub <meghade2003@gmail.com>
2026-01-28 21:23:25 +0530

    Practice implement single LL

--- Data Structure/LinkList/implement_LL.java
+++ Data Structure/LinkList/implement_LL.java
@@ -5,6 +5,28 @@ public class implement_LL {
     Node(int data){
       this.data=data;
     }
+
+    public static class Linkedlist{
+      
+    }
+    void insert_big(){
+
+    }
+    void insert_end(){
+
+    }
+    void insert_at_idx(){
+
+    }
+    void del_at_big(){
+
+    }
+      void del_at_end(){
+      
+    }
+      void del_at_idx(){
+      
+    }
   }
   
   public  static void main(String[] args){
